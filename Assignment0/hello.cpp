7129d77c9477b689e599034e47f1d75dc873a5b3
everettprussak <prussak3@aol.com>
2022-01-03 13:16:55 -0800

    Adds Hello World

---
+++ Assignment0/hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+  cout << "Hello World!" << endl; //end line is new line character or \n in string works
+
+  return 0;
+}
+
+//g++ -std=c++11 hello.cpp -o hello.o
+//./hello.o
